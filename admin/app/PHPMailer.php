721b96f4ff2089151f31f0210a63ced6b0d3ab23
hugo MARC <hugo.marc76113@gmail.com>
2021-09-13 19:50:55 +0200

    MAJ PHPMAILER;

--- admin/app/PHPMailer.php
+++ admin/app/PHPMailer.php
@@ -3,19 +3,32 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'src/Exception.php';
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$smtpUsername = 'hmarc@normandiewebschool.fr';
+$smtpPassword = 'MDPtestSENDmailWITHPHPMAILERtest76*/*';
+
+$emailFrom ='contact@hugo-marc.xyz';
+$emailFromName = 'Hugo MARC';
+
+$emailTo = 'hugo.marc76113@gmail.com';
+$emailToName = 'TEST';
+
 
 $mail = new PHPMailer;
 $mail->isSMTP(); 
 $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+
 $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
 $mail->Port = 587; // TLS only
 $mail->SMTPSecure = 'tls'; // ssl is depracated
 $mail->SMTPAuth = true;
 $mail->Username = $smtpUsername;
 $mail->Password = $smtpPassword;
+
+
 $mail->setFrom($emailFrom, $emailFromName);
 $mail->addAddress($emailTo, $emailToName);
 $mail->Subject = 'PHPMailer GMail SMTP test';
