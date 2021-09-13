9f2ddb662e792ec86a25d17a4aedf5036d02a7ae
hugo MARC <hugo.marc76113@gmail.com>
2021-09-13 19:56:11 +0200

    MAJ

--- admin/app/PHPMailer.php
+++ admin/app/PHPMailer.php
@@ -7,7 +7,7 @@ error_reporting( E_ALL );
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMailer/src/Exception.php';
+require '../SNPA/PHPMailer/src/Exception.php';
 require '../PHPMailer/src/PHPMailer.php';
 require '../PHPMailer/src/SMTP.php';
 
