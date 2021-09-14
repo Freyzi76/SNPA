5187dcd70a29a189e7a9d4c873fd5f17cd0c9979
hugo MARC <hugo.marc76113@gmail.com>
2021-09-15 01:18:54 +0200

    MAJ

--- admin/templates/headerNew.php
+++ admin/templates/headerNew.php
@@ -9,43 +9,29 @@
         </button>
 
             <div class="collapse navbar-collapse" id="navbarsExample04">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                            <li class="nav-item">
 
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
 
-                    </li>
+                            </li>
 
-                    <li class="nav-item">
+                            <li class="nav-item">
 
-                        <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="#">Link</a>
 
-                    </li>
+                            </li>
 
-                    <li class="nav-item">
+                            <li class="nav-item">
 
-                        <a class="nav-link disabled">Disabled</a>
+                                <a class="nav-link disabled">Disabled</a>
 
-                    </li>
+                            </li>
 
-                    <li class="nav-item dropdown">
-
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-
-                        <ul class="dropdown-menu" aria-labelledby="dropdown04">
-
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-
+                            
                         </ul>
-                        </li>
-                        </ul>
-                        
-                    </div>
-                </div>
+
+
             <div>
 
     </div>
