a9791b3318490ff9f99d3b2d7214b756799c6f72
hugo MARC <hugo.marc76113@gmail.com>
2021-09-14 15:03:52 +0200

    TEST

--- admin/deploy/deploy.php
+++ admin/deploy/deploy.php
@@ -6,6 +6,7 @@ error_reporting( E_ALL );
 
 
 echo 'test1';
-$output = shell_exec('sudo /home/hugo76113/script.sh');
+$output = shell_exec('sudo cd /var/www/');
+$output. = shell_exec('sudo rm -r SNPA');
 echo "<pre>$output</pre>";
 ?>
