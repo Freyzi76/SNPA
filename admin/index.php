9b80390d43fc17d5734039080e77809ccd9451dd
hugo MARC <hugo.marc76113@gmail.com>
2021-09-14 23:46:42 +0200

    MAJ

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', '-1');
+error_reporting( E_ALL );
   // Permet de savoir s'il y a une session. 
 
   // C'est-à-dire si un utilisateur s'est connecté à votre site 
