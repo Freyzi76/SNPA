285a69c9266ee48bf706bbb755bb3ebd90a5c6a3
hugo MARC <hugo.marc76113@gmail.com>
2021-09-15 00:02:48 +0200

    MAJ

--- admin/index.php
+++ admin/index.php
@@ -33,42 +33,9 @@ error_reporting( E_ALL );
 
     <?= require('templates/Header.php'); ?>
 
-    <?php
+    <?= require('pages/index.php'); ?>
 
-      if(!isset($_SESSION['id'])) {  
-
-          if($_SESSION['SP'] == 1) {
-
-            ?>
-
-            <a href="add-admin.php">Ajouter un Administrateur</a>
-            <br>
-            <a href="manage-admin.php">Gerez Les Administrateur</a>
-            <br>
-
-           
-
-
-          <?php
-
-          }
-
-          ?>
-
-
-
-          <a href="add-item.php">Ajouter un produit</a>
-
-          
-
-
- 
-
-      <?php
-        
-      } 
-
-      ?>
+    
 
   </body>
 
---
+++ admin/pages/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    if(!isset($_SESSION['id'])) {  
+
+        if($_SESSION['SP'] == 1) {
+
+?>
+
+            <a href="add-admin.php">Ajouter un Administrateur</a>
+            <br>
+            <a href="manage-admin.php">Gerez Les Administrateur</a>
+            <br>
+
+           
+
+
+        <?php
+
+        }
+
+        ?>
+
+
+
+        <a href="add-item.php">Ajouter un produit</a>
+
+          
+
+
+ 
+
+    <?php
+            
+    } 
+
+    ?>
