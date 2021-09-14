0eb835d82dd9b96a2760b2938fcdbe4bc97d50b8
hugo MARC <hugo.marc76113@gmail.com>
2021-09-14 18:04:08 +0200

    MAJ

--- PHPMailer/PHPMailer.php
+++ PHPMailer/PHPMailer.php
@@ -78,7 +78,7 @@
                             $mail->isSMTP(); 
                             $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
                             
-                            $mail->Host =gethostbyname('smtp.gmail.com'); // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+                            $mail->Host = 'smtp.gmail.com'; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
                             $mail->Port = 587; // TLS only
                             $mail->SMTPSecure = 'tls'; // ssl is depracated
                             $mail->SMTPAuth = true;
