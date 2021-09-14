e34887ad3e8ff8779773b7a0c14f712a8a2a3f01
hugo MARC <hugo.marc76113@gmail.com>
2021-09-14 03:01:44 +0200

    redirection oublier et exit;
    problème corriger

--- PHPMailer/PHPMailer.php
+++ PHPMailer/PHPMailer.php
@@ -93,7 +93,8 @@
                             $mail->AltBody = 'HTML messaging not supported';
                             // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
                             
-                          
+                            header('Location: ../admin/index.php');
+                            exit;
 
                     }   
                 }       
