6b5d84b56f9dc063bd019837d57bd975d83330b1
hugo MARC <hugo.marc76113@gmail.com>
2021-09-14 17:39:48 +0200

    MAJ

--- PHPMailer/PHPMailer.php
+++ PHPMailer/PHPMailer.php
@@ -71,14 +71,14 @@
 
                             $smtpUsername = 'hmarc@normandiewebschool.fr';
                             
-                            $emailFrom ='contact@hugo-marc.xyz';
+                            $emailFrom ='hmarc@normandiewebschool.fr';
                             $emailFromName = 'Hugo MARC SNPA';
                             
                             $mail = new PHPMailer;
                             $mail->isSMTP(); 
                             $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
                             
-                            $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+                            $mail->gethostbyname('smtp.gmail.com'); // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
                             $mail->Port = 587; // TLS only
                             $mail->SMTPSecure = 'tls'; // ssl is depracated
                             $mail->SMTPAuth = true;
