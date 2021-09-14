78eca559e8ada93599a934313e54e1c06fd5ca69
hugo MARC <hugo.marc76113@gmail.com>
2021-09-15 00:32:28 +0200

    MAJ

--- admin/templates/loginTemplate.php
+++ admin/templates/loginTemplate.php
@@ -1,45 +1,56 @@
-<form method="post" class="container-fluid formcontainer">
+
+<div class="text-center">
+    
+    <div class="form-signin">
+
+        <form method="post" class="container-fluid formcontainer">
 
 
 
-    <?php
-    if (isset($er_mail)){
-    ?>
+            <?php
+            if (isset($er_mail)){
+            ?>
 
-        <div><?= $er_mail ?></div>
+                <div><?= $er_mail ?></div>
 
-    <?php   
-    }
-    ?>
-
-
-        <div class="mb-3"> 
-            
-            <label class="form-label">Email</label>
-            
-            <input class="form-control" type="email" placeholder="Adresse mail" name="mail" value="<?php if(isset($mail)){ echo $mail; }?>" required>
-        
-        </div>
+            <?php   
+            }
+            ?>
 
 
-    <?php
-    if (isset($er_mdp)){
-    ?>
-
-        <div><?= $er_mdp ?></div>
-
-
-    <?php   
-    }
-    ?>
-
-        <div class="mb-3"> 
-            <label class="form-label">Email</label>
-            
-            <input class="form-control" type="password" placeholder="Mot de passe" name="mdp" value="<?php if(isset($mdp)){ echo $mdp; }?>" required>
-
-            <button class="btn btn-primary " type="submit" name="connexion">Se connecter</button>
-        </div>
+                <div class="mb-3"> 
+                    
+                    <label class="form-label">Email</label>
+                    
+                    <input class="form-control" type="email" placeholder="Adresse mail" name="mail" value="<?php if(isset($mail)){ echo $mail; }?>" required>
+                
+                </div>
 
 
-</form>
+            <?php
+            if (isset($er_mdp)){
+            ?>
+
+                <div><?= $er_mdp ?></div>
+
+
+            <?php   
+            }
+            ?>
+
+                <div class="mb-3"> 
+                    <label class="form-label">Email</label>
+                    
+                    <input class="form-control" type="password" placeholder="Mot de passe" name="mdp" value="<?php if(isset($mdp)){ echo $mdp; }?>" required>
+
+                    <button class="btn btn-primary " type="submit" name="connexion">Se connecter</button>
+                </div>
+
+
+        </form>
+
+    </div>
+
+</div>
+
+
