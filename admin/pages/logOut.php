de5ddc2a8c32613d7d79e2e92f285d9e3dafc28c
hugo MARC <hugo.marc76113@gmail.com>
2021-09-15 15:59:58 +0200

    TEST

--- admin/pages/logOut.php
+++ admin/pages/logOut.php
@@ -2,7 +2,9 @@
 
   session_destroy();
 
-  //header('location: http://hugo-marc.xyz/admin?page=home'); // la page sur lequel l'utilisateur sera redirigé.
+  echo ' Destroy ';
+
+  header('location: http://hugo-marc.xyz/admin?page=home'); // la page sur lequel l'utilisateur sera redirigé.
 
   exit;
 
